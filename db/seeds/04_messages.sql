866eaebb198cc1b9670b809c5f626e085edd80fa
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-04-29 17:14:03 -0400

    Update message content

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,10 +1,26 @@
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(4,10,8,'Z8i2AXa94chYxSdoHgE3VCtq7ue7fYn70NlkM84CqKpOHO62Aw');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(6,13,7,'myXO25Q3ddgykikq3bImCMKWfisxl7tH80OZOVTDnJNrJZhqWL');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(2,7,7,'CFZnAtTNcRguYztTLciHF829dNGBCba1Y86uyvNmAzL2r6ukxW');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(4,14,27,'MDEMaJxnRLB0pVa39eZO6HPxAShcfOpPrTL52hYma9soJBBkUq');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(16,17,39,'elExQLImVHDfvJVKeAbGxy7Zln9Q6IyaxdMsGIlWP9F2oIX2qa');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(16,13,18,'v9qnoMnM2qNSmsS6nDWZF0xsFvcQHjfSIECegEc58KXT2kBZF6');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(1,8,23,'QR4L7i3tXFSK4JiqxZilaa4cd0ftCKKgHz2wHSifiMlEFJ7act');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(6,10,25,'wCzWbsHDJrM4vDEQwje1CCOzzXU2ERT2oCUxCapjbbYeYKUF3J');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(14,4,40,'pbuxAgEYO92YUPssROW34akouapCO94kIE9kkAC3lLMcfGE4lX');
-INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(12,18,17,'Q2P0GDj1BKPFSPPQIIRLcStNIInWub1X8Q2Mtte6dCjB6XN0yo');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(4,10,8,'Lorem ipsum dolor sit amet. Est consequatur galisum sit nihil fugiat est fugiat voluptas non enim sint et veritatis nesciunt ut exercitationem asperiores eos odio voluptas. Est labore dolore qui quam voluptas quo maxime repudiandae qui dolorem cumque ut quidem unde. Ut error veniam eos velit modi aut corrupti quia ex corporis galisum! Aut sequi tenetur et saepe dolore At sequi corporis eum cupiditate earum At molestias corrupti et nulla culpa!');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(6,13,7,'Lorem ipsum dolor sit amet. Non facilis unde id autem totam quo nemo dolorem. Nam earum voluptas aut repellendus ratione ab praesentium optio. Non voluptatem esse At quia possimus qui eligendi sint. At enim quasi rem veniam pariatur in praesentium voluptate ut velit voluptate qui ullam facere et dicta quod.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(2,7,7,'
+
+Lorem ipsum dolor sit amet. Sit laborum beatae in sunt temporibus At vitae earum ex amet debitis aut fuga expedita qui itaque tenetur. Aut repudiandae praesentium eum distinctio autem id ducimus facilis a voluptatem aliquid. Quo fugit odio aut laudantium perspiciatis ut labore consectetur! Est sint modi vel minus fuga qui exercitationem nisi qui quia inventore et vitae accusantium.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(4,14,27,'
+
+Lorem ipsum dolor sit amet. Ut mollitia quia est necessitatibus corporis et voluptates quisquam ut placeat consequatur non ullam illo aut saepe voluptas vel atque delectus. Aut suscipit nisi sed temporibus eaque non odit dolorem aut voluptatum odit ea optio similique. Eos recusandae harum ut magnam voluptas hic quae iste. Vel vitae dolore ad laborum commodi et dolorem ipsum eos dolor dolor?');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(16,17,39,'
+
+Lorem ipsum dolor sit amet. Ut optio repellat et soluta excepturi et culpa rerum sit quia ratione ut cumque fugiat quo magni animi hic dignissimos dolorum. Ea laboriosam ullam quo quidem explicabo et maiores deleniti ut cumque nobis cum vero atque et aspernatur tempora. Ea rerum vero qui voluptatibus temporibus sit debitis officiis ad unde dolor ea similique eligendi aut officia itaque hic omnis voluptate.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(16,13,18,'
+
+Lorem ipsum dolor sit amet. At inventore recusandae non voluptates cupiditate et illum quia qui possimus alias. Est dolor culpa hic ipsam amet ex odio aspernatur. Aut delectus impedit eum sapiente excepturi sit quibusdam maxime in quia laudantium et galisum accusantium sed officiis quidem et pariatur Quis. Aut enim iure est velit eaque et enim alias aut dignissimos optio.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(1,8,23,'
+
+Lorem ipsum dolor sit amet. Et explicabo ipsum in perferendis neque non itaque fuga ut perferendis accusamus et Quis quia in dolorem nisi aut placeat laboriosam? Et consequatur sint id ipsam assumenda cum rerum iure eos saepe beatae. Sed ullam cupiditate et perspiciatis tenetur eos maiores pariatur est quibusdam dolor At iusto quisquam et officia eveniet. Est magnam mollitia sed dolorem dignissimos ut quibusdam fuga in natus nihil aut architecto dolore.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(6,10,25,'
+
+Lorem ipsum dolor sit amet. Qui laborum illo non repellat veniam ut earum sunt qui placeat quis ut ipsa fugit ab quas nisi! Ut earum officiis eos iure eaque hic ipsa laborum qui quisquam nemo est dolorum sint vel pariatur officiis At perferendis Quis. Ex natus dolorem et nihil galisum nam autem eligendi et dolorem perferendis et minus omnis. Sed expedita vitae ex iste corporis ut autem magni eos beatae soluta et dolor repudiandae aut quis exercitationem qui velit explicabo.');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(14,4,40,'
+
+Lorem ipsum dolor sit amet. Sed voluptas rerum ex porro sapiente et numquam quas aut animi rerum vel harum maiores ea optio Quis ea autem distinctio. Aut nesciunt voluptatum et distinctio accusamus non itaque quia qui explicabo galisum est nemo voluptatum!');
+INSERT INTO messages (sender_id, receiver_id, listing_id, content) VALUES(12,18,17,'
+
+Lorem ipsum dolor sit amet. Quo nisi quia aut suscipit iusto non expedita iure aut voluptas illum sit repudiandae natus. Sed perferendis molestiae ad incidunt galisum in mollitia necessitatibus eos laboriosam architecto ut maxime dolorem. Ad soluta dolore sed corrupti beatae qui suscipit molestiae est numquam adipisci aut nihil aperiam et velit voluptatem. Et quam doloremque in voluptatem deserunt ad soluta veniam id molestiae numquam.');
