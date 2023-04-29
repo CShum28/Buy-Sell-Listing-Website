9000545bb2b521dce9b3a3b0cfecb05249643698
TheLoneliestTime <john.a.roxas@gmail.com>
2023-04-28 19:13:11 -0700

    need to grab updated database

--- routes/all-listings.js
+++ routes/all-listings.js
@@ -14,7 +14,18 @@ router.get("/listings", async (req, res) => {
   try {
     // Get a client frmo connection pool
     const client = await pool.connect();
-  };
+
+    // SQL query to retrieve all the rows from the table titled 'listings'
+    const result = await client.query('SELECT * FROM listings');
+
+    // grab ONLY the rows
+    const listings = result.rows;
+
+    // Release the client. Need to use release instead of pool.end(), caused lots of problems!
+    client.release();
+  }
+
+
   res.render("listingpage");
 });
 
