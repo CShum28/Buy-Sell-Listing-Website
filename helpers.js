a581d047a9b3b39463d45cc31ed07d7890f5f602
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-04-30 01:37:30 -0400

    Use database instead of pool

--- helpers.js
+++ helpers.js
@@ -1,17 +1,20 @@
 const { Pool } = require("pg");
 const database = require("./db/connection");
 
+
 //Get user by username
 const getUserByUsername = function (username) {
-  return Pool.
-    query(`SELECT * FROM users WHERE username = $1`, [username])
+    return database
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
     .then((result) => {
+      console.log(result.rows[0][username]);
       return result.rows[0];
     })
     .catch((err) => {
-      console.log(err.message)
+      console.log(err.message);
     });
-
 };
+
+getUserByUsername("jp");
 
 module.exports = { getUserByUsername };
