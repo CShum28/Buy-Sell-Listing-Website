19d41fe40d0aa7c8961331ba40406f67b8a0db72
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-04-30 13:55:41 -0400

    Uncomment

--- helpers.js
+++ helpers.js
@@ -8,18 +8,18 @@ Please don't touch this file. I will come to it tomorrow.
 */
 
 //Get user by username
-// const getUserByUsername = function (username) {
-//     return database
-//     .query(`SELECT * FROM users WHERE username = $1`, [username])
-//     .then((result) => {
-//       console.log(result.rows[0][username]);
-//       return result.rows[0];
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
+const getUserByUsername = function (username) {
+    return database
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then((result) => {
+      console.log(result.rows[0][username]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
-// getUserByUsername("jp");
+getUserByUsername("jp");
 
-// module.exports = { getUserByUsername };
+module.exports = { getUserByUsername };
