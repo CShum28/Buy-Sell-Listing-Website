2aaa3a21b806b6e7ff9ce98f53f0cd85a7b29be1
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-04-30 00:48:26 -0400

    Define db connection in one place and require it in other files

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,22 @@
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const dbParams = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 };
 
-const db = new Pool(dbParams);
+const database = new Pool(dbParams);
 
-db.connect();
+// db.connect();
 
-module.exports = db;
+const getUsers = () => {
+  return database.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { database, getUsers };
