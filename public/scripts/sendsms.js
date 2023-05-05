d4108c21aa22b55d68d1b68d5bfdb237c32b77ce
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-05 16:26:32 -0700

    add jquery logic for pressing the send sms button

---
+++ public/scripts/sendsms.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+  $(".sendsmsbutton").on("click", async (event) => {
+    // Perform SMS sending logic here
+    alert("Sending SMS...");
+    // Add your Twilio API code to send the SMS
+  });
+});
