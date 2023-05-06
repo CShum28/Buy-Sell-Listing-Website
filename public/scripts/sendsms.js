bae4dfb008335ce5fc940616c1dbbcd747fa8944
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-05 17:25:25 -0700

    it's like 50% LOL

--- public/scripts/sendsms.js
+++ public/scripts/sendsms.js
@@ -1,7 +1,38 @@
 $(document).ready(() => {
-  $(".sendsmsbutton").on("click", async (event) => {
-    // Perform SMS sending logic here
-    alert("Sending SMS...");
-    // Add your Twilio API code to send the SMS
+  $(".sendsmsbutton").on("click", (event) => {
+    event.preventDefault();
+    // alert("You hit!");
+    $("#messageBox").addClass("show");
+  });
+
+  $("[id^='sendMessageBtn-']").on("click", async (event) => {
+    event.preventDefault();
+    const messageBoxId = $(event.target).attr("data-messageboxid");
+    const messageBody = $(`#messageBody-${messageBoxId}`).val();
+
+    try {
+      const response = await fetch("/send-sms", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messageBody }),
+      });
+
+      if (response.ok) {
+        // Message sent successfully
+        alert("SMS message sent successfully.");
+      } else {
+        // Error occurred while sending the message
+        alert("An error occurred while sending the SMS message.");
+      }
+    } catch (error) {
+      // Handle network or other errors
+      console.error(error);
+      alert("An error occurred while sending the SMS message.");
+    }
+
+    $(`#messageBox-${messageBoxId}`).removeClass("show");
+    $(`#messageBody-${messageBoxId}`).val("");
   });
 });
